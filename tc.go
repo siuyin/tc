e4f0686fd3069dcd586a56141f86e48df5780e3c
Loh Siu Yin <siuyin@beyondbroadcast.com>
2019-08-15 07:16:23 +0000

    Added N25 NonDropFrame PAL-rate timecode.

--- tc.go
+++ tc.go
@@ -8,9 +8,16 @@ import (
 )
 
 const (
-	frameTimeNS = 1001 * 1000000000 / 30000
-	tDF29_97    = time.Duration(1001 * 1000000000 / 30000)
+	tDF29_97 = time.Duration(1001 * 1000000000 / 30000)
+	tN25     = time.Duration(1000000000 / 25)
 )
+
+type TC interface {
+	Add(tc *TC) *TC
+	Sub(tc *TC) *TC
+	Dur() time.Duration
+	String() string
+}
 
 func validTC(tc string) bool {
 	if len(tc) != 11 {
@@ -19,14 +26,76 @@ func validTC(tc string) bool {
 	return true
 }
 
+// N25 represent 25 fps Non-drop frame (ie. PAL rate)
+type N25 struct {
+	tc string
+}
+
+func NewN25(tc string) *N25 {
+	if !validTC(tc) {
+		return &N25{"00:00:00:00"}
+	}
+	tc = tc[0:8] + ":" + tc[9:11]
+	return &N25{tc}
+}
+
+// FrameCount returns an N25 timecode frame count.
+func (c *N25) FrameCount() (int, error) {
+	hh, err := strconv.Atoi(c.tc[0:2])
+	if err != nil {
+		return 0, err
+	}
+	mm, err := strconv.Atoi(c.tc[3:5])
+	if err != nil {
+		return 0, err
+	}
+	ss, err := strconv.Atoi(c.tc[6:8])
+	if err != nil {
+		return 0, err
+	}
+	ff, err := strconv.Atoi(c.tc[9:11])
+	if err != nil {
+		return 0, err
+	}
+	return hh*3600*25 + mm*60*25 + ss*25 + ff, nil
+}
+
+func NewN25FrameCount(fc int) *N25 {
+	hh := fc / (3600 * 25)
+	mm := (fc - hh*3600*25) / (60 * 25)
+	ss := (fc - hh*3600*25 - mm*60*25) / 25
+	ff := fc - hh*3600*25 - mm*60*25 - ss*25
+	return NewN25(fmt.Sprintf("%02d:%02d:%02d:%02d", hh, mm, ss, ff))
+}
+
+// Add adds timecodes c + tc.
+func (c *N25) Add(tc *N25) *N25 {
+	fc1, _ := c.FrameCount()
+	fc2, _ := tc.FrameCount()
+	return NewN25FrameCount(fc1 + fc2)
+}
+
+// Sub subtracts timecodes c - tc.
+func (c *N25) Sub(tc *N25) *N25 {
+	fc1, _ := c.FrameCount()
+	fc2, _ := tc.FrameCount()
+	return NewN25FrameCount(fc1 - fc2)
+}
+
+// Dur return the duration of the timecode.
+func (c *N25) Dur() time.Duration {
+	fc, _ := c.FrameCount()
+	return time.Duration(fc) * tN25
+}
+
+// String fulfills the Stringer interface
+func (c *N25) String() string {
+	return c.tc
+}
+
 // DF29_97 represents a dropframe timecode
 type DF29_97 struct {
 	tc string
-}
-type TC interface {
-	Add(tc *TC) *TC
-	Sub(tc *TC) *TC
-	Dur() time.Duration
 }
 
 // NewTC returns a reference to a new dropframe timecode.
--- tc_test.go
+++ tc_test.go
@@ -1,6 +1,58 @@
 package tc
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
+
+func TestFrameCountN25(t *testing.T) {
+	dat := []struct {
+		i string
+		o int
+	}{
+		{i: "00:10:45:21", o: 16146},
+	}
+	for i, d := range dat {
+		tc := NewN25(d.i)
+		o, err := tc.FrameCount()
+		if err != nil {
+			t.Error(err)
+		}
+		if o != d.o {
+			t.Errorf("case %d: unexpected value: %d", i, o)
+		}
+	}
+}
+func TestSubN25(t *testing.T) {
+	dat := []struct {
+		a, b string
+		o    string
+	}{
+		{"10:10:45:00", "00:00:00:01", "10:10:44:24"},
+		{"10:10:45:01", "00:00:00:01", "10:10:45:00"},
+	}
+	for i, d := range dat {
+		o := NewN25(d.a).Sub(NewN25(d.b))
+		if o.String() != NewN25(d.o).String() {
+			t.Errorf("case %d: Unexpected value: %s", i, o)
+		}
+	}
+}
+
+func TestDurN25(t *testing.T) {
+	dat := []struct {
+		a string
+		o time.Duration
+	}{
+		{"12:34:56:24", (12*3600*25 + 34*60*25 + 56*25 + 24) * tN25},
+	}
+	for i, d := range dat {
+		o := NewN25(d.a)
+		if o.Dur() != d.o {
+			t.Errorf("case %d: Unexpected value: %d", i, o.Dur())
+		}
+	}
+}
 
 func TestFrameCountDF29_97(t *testing.T) {
 	dat := []struct {
